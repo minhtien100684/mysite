5acc426a3eb63dc7513cd1dcd514617d5bec1572
Tien Minh Nguyen <tiennm6@vng.com.vn>
2023-04-27 15:05:57 +0700

    gitlab ci update

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
@@ -41,3 +41,6 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+def owner(request):
+    return HttpResponse("Hello, world. c7d3bff1 is the polls owner.")
